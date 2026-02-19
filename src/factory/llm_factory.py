a80dc8af37ca8a445045060d3816c528188131fa
EnzoChris <enzojoaquim85@gmail.com>
2026-02-19 18:49:55 -0300

    REFACTOR: Criando o método get_embedding para acessar o modelo de embedding através do arquivo factory/llm_factory

--- src/factory/llm_factory.py
+++ src/factory/llm_factory.py
@@ -1,7 +1,11 @@
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 def get_model():
     return ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=os.getenv("CHAVE_API"), temperature=0)
+
+def get_embedding():
+    return GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", api_key=os.getenv("CHAVE_API"))
