3a2041335f24555c1a6a0ffb5e971a44af538f36
EnzoChris <enzojoaquim85@gmail.com>
2026-02-19 21:04:38 -0300

    FEATURE: Terminando a aplicação em telas .html utilizando FastAPI e Jinja2

--- src/application/main.py
+++ src/application/main.py
@@ -1,22 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from state.state import AgentState
+from pathlib import Path
 from main import retornar_resp
 
 
 server = FastAPI()
 
-
-@server.get("/")
-def home():
-    return {"mensagem": "O chatbot chrisSeek está rodando! 🚀"}
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
-@server.get("/state/pergunta/{pergunta}")
-def perguntar_api(pergunta: str):
+@server.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
+
+
+#@server.get("/state/pergunta/{pergunta}")
+#def perguntar_api(pergunta: str):
+#    resposta = retornar_resp(pergunta)
+#    return {
+#        "pergunta": pergunta,
+#        "resposta": resposta
+#    }
+
+@server.post("/perguntar", response_class=HTMLResponse)
+def perguntar_fast(request:Request, pergunta: str = Form(...)):
     resposta = retornar_resp(pergunta)
 
-    return {
-        "pergunta": pergunta,
-        "resposta": resposta
-    }
+    return templates.TemplateResponse(
+        "resposta.html",
+        {
+        "request":request,
+        "pergunta":pergunta,
+        "resposta":resposta
+        }
+    )
